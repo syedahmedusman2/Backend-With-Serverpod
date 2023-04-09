bf54856dd56a8b3e2b0cd08bcb754d30ef9afa28
Ahmed Usman <ahmedusmanpakistan@gmail.com>
2023-04-10 01:28:44 +0500

    crud application

--- mypod_flutter/lib/main.dart
+++ mypod_flutter/lib/main.dart
@@ -63,12 +63,7 @@ class MyHomePageState extends State<MyHomePage> {
   }
   
 
-  addproduct()async{
-      var product = Product(title: "My Product",
-     price: 200,
-    description: "This the the description",
-     unit: 12,
-      isAvailable: true);
+  addproduct(Product product)async{
     try{
 
      var result = await client.product.insertProduct(product);
@@ -76,6 +71,54 @@ class MyHomePageState extends State<MyHomePage> {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Product added successfuly")));
       setState(() {
         _resultMessage = "Article Added";
+        _products.add(product);
+      });
+      Navigator.pop(context);
+     }
+    }on Exception catch(e){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("There is an error $e")));
+
+    }
+  }
+  List<Product> _products = [];
+  fetchProduct({search})async{
+    try{
+     var result = await client.product.getProducts(search: search);
+     if(result.isNotEmpty){
+      setState(() {
+        _products = result;
+        _resultMessage = "${result.length} products in the table";
+      });
+     }
+    }on Exception catch(e){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("There is an error $e")));
+
+    }
+  }
+  updateProduct(Product product)async{
+    try{
+     var result = await client.product.updateProduct(product);
+     if(result){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Product update successfuly")));
+     }
+     Navigator.pop(context);
+     setState(() {
+       int index = _products.indexWhere((element) => element.id==product.id);
+       _products[index] = product;
+     });
+    }on Exception catch(e){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("There is an error $e")));
+
+    }
+  }
+  deleteProduct(id)async{
+    try{
+     var result = await client.product.deleteProduct(id);
+     if(result){
+      print(result);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Product deleted successfuly")));
+      setState(() {
+        _products.removeWhere((element) => element.id==id);
       });
      }
     }on Exception catch(e){
@@ -84,19 +127,10 @@ class MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  fetchProduct()async{
-    try{
-     var result = await client.product.getProducts();
-     if(result.isNotEmpty){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Product fetch successfuly")));
-      setState(() {
-        _resultMessage = "${result.length} products in the table";
-      });
-     }
-    }on Exception catch(e){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("There is an error $e")));
-
-    }
+  @override
+  void initState() {
+    fetchProduct();
+    super.initState();
   }
   @override
   Widget build(BuildContext context) {
@@ -106,31 +140,101 @@ class MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: TextField(
-                controller: _textEditingController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your name',
-                ),
+        child: ListView.builder(
+          itemCount: _products.length,
+          itemBuilder: (BuildContext context, index){
+            return ListTile(
+              title: Text(_products[index].title),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("${_products[index].price} \$"),
+                  Text("${_products[index].unit} Units"),
+                ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: ElevatedButton(
-                onPressed: fetchProduct,
-                child: const Text('Send to Server'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(onPressed: (){
+                    deleteProduct(_products[index].id);
+                  }, icon: Icon(Icons.delete)),
+                  IconButton(onPressed: (){
+                    _showArticleDialog(product: _products[index]);
+                  }, icon: Icon(Icons.edit))
+
+                ],
               ),
-            ),
-            _ResultDisplay(
-              resultMessage: _resultMessage,
-              errorMessage: _errorMessage,
-            ),
-          ],
-        ),
+            );
+          })
       ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        _showArticleDialog();
+      },
+      child: Icon(Icons.add),),
+    );
+  }
+  _showArticleDialog({Product? product}) {
+    var titleController = TextEditingController();
+    var priceController = TextEditingController();
+    var unitController = TextEditingController();
+
+    if (product != null) {
+      titleController.text = product.title;
+      priceController.text = product.price.toString();
+      unitController.text = product.unit.toString();
+    }
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter title',
+                  ),
+                ),
+                TextField(
+                  controller: priceController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Price',
+                  ),
+                ),
+                TextField(
+                  controller: unitController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Unit',
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    if (product != null) {
+                      product.title = titleController.text;
+                      product.price = double.parse(priceController.text);
+                      updateProduct(product);
+                    } else {
+                      var newProduct = Product(
+                        title: titleController.text,
+                        price: double.parse(priceController.text),
+                        unit: int.parse(unitController.text),
+                        isAvailable: true,
+                        description: ""
+                      );
+                      addproduct(newProduct);
+                      
+                    }
+                  },
+                  child: const Text('Add Article'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
